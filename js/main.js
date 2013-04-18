0b1bc1eceab66d7b8f05d58fa1c4a3513a0440cc
eng618 <eng618@fullsail.edu>
2013-04-18 08:32:24 -0400

    moved js link to bottom of body tab in both .html files, with other various optimizations. Created multiple functions in main.js to save and restore data

--- js/main.js
+++ js/main.js
@@ -1,3 +1,37 @@
 //Eric Garcia
 //VFW 1304
 //Project2
+
+window.addEventListener('DOMContentLoaded', function(){
+
+	//Variables
+	var occasion = document.getElementById('occasion');
+	var date = document.getElementById('date');
+	var importance = document.getElementById('importance');
+	var sharedWith = document.getElementById('form').sharedWith;
+	var description = document.getElementById('description');
+
+	//getElementById Function
+	function getElement(x){
+		var elementRequested = document.getElementById(x);
+		return elementRequested
+	};
+
+	var saveData = function(){
+		localStorage.setItem("Occasion", occasion.value);
+	};
+
+	var restoreData = function(){
+		var occasionKey = localStorage.key('Occasion');
+		var occasionValue = localStorage.getItem(occasionKey);
+		occasion.value = occasionValue
+	};
+
+	occasion.addEventListener("blur", saveData);
+
+
+	restoreData();
+
+
+ 
+});

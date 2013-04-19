d6f6e47d96b17666ccf1bd43b539279665020860
eng618 <eng618@fullsail.edu>
2013-04-18 21:33:31 -0400

    refactored main.js, added function getCheckedValues, clearData

--- js/main.js
+++ js/main.js
@@ -1,25 +1,29 @@
-//Eric Garcia
-//VFW 1304
-//Project2
+// Eric Garcia
+// VFW 1304
+// Project 2
 
 window.addEventListener('DOMContentLoaded', function(){
 
-	//getElementById Function
-	function $(x){
-		var elementRequested = document.getElementById(x);
-		return elementRequested;
-	};
-/*
+	/*
 	//Variables
 	var occasion = document.getElementById('occasion');
 	var date = document.getElementById('date');
 	var importance = document.getElementById('importance');
 	var sharedWith = document.getElementById('form').sharedWith;
 	var description = document.getElementById('description');
-*/
+	*/
+	// var submit = document.getElementById('save');
+
+	// getElementById Function
+	function $(x){
+		var elementRequested = document.getElementById(x);
+		return elementRequested;
+	};
+
+
 	
 
-	//Functions
+	// Functions
 	var saveData = function(){
 		localStorage.setItem('Occasion', occasion.value);
 	};
@@ -29,19 +33,30 @@ window.addEventListener('DOMContentLoaded', function(){
 		var occasionValue = localStorage.getItem(occasionKey);
 		occasion.value = occasionValue;
 	};
+	var clearData = function(){
+		localStorage.clear;
+	};
+	// var saveForm = function(){};
 
-	//Display data, Clear data, submit links
-	var displaySavedData = $('displaySavedData');
-	var clearSavedData = $('clearData');
+
+	// Display data, Clear data, submit links
+	var loadSavedData = $('loadSavedData');
+	var clearSavedData = $('clearSavedData');
 	var save = $('save');
-	displaySavedData.addEventListener('click', displayData);
+	loadSavedData.addEventListener('click', loadData);
 	clearSavedData.addEventListener('click', clearData);
-	save.addEventListener('click', saveForm)
+	// save.addEventListener('click', saveForm)
 
+	//Check-box array
+	var getCheckedValues = function(){
+		var checkboxes = document.getElementById('form').sharedWith;
+		for (i=0; i<checkboxes.length; i++){
+			if(checkboxes[i].checked ){
+				console.log(checkboxes[i].value);
+			};
+		};
+	};
 
-
-	loadData();
-
-
+	save.addEventListener('click', getCheckedValues())
  
 });
